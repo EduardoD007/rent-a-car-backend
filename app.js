76a35dc9013c72eb8d0b85f5f40ed54e61e024c8
Eduardo Dias <edufreitas205@gmail.com>
2025-04-26 10:45:38 -0300

    altera cors

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(cors());
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 
 routes(app);
 
