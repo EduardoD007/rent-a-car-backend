8313a05f8503e6deb2c0414679988793ae484685
Eduardo Dias <edufreitas205@gmail.com>
2025-04-28 11:50:37 -0300

    deleta vercel.json

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const cors = require('cors')
 
 const app = express();
 
-const corsConfig = {
-  origin: '',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+app.use(cors());
 
 routes(app);
 
